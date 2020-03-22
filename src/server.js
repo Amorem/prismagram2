e57ebd200a97b0c8bb6a204b09570cd168a5e9cb
Amorem <dev@amorem.com>
2020-03-22 17:50:39 +0100

    sendSecretEmail OK

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import { sendSecretMail } from "./utils";
+
+sendSecretMail("cedric@amorem.com", "coucou");
 
 const PORT = process.env.PORT || 4000;
 
