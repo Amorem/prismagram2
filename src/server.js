4784756bdb2cc39b6f661497222e7033e5fef9d7
Amorem <dev@amorem.com>
2020-03-23 16:44:19 +0100

    Passport : OK

--- src/server.js
+++ src/server.js
@@ -4,12 +4,17 @@ import logger from "morgan";
 import passport from "passport";
 import schema from "./schema";
 import "./passport";
+import { authenticateJwt } from "./passport";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({
+  schema,
+  context: ({ request }) => ({ request })
+});
 
 server.express.use(logger("dev"));
+server.express.use(authenticateJwt);
 
 server.start({ port: PORT }, () =>
   console.log(`✔ Server running http://localhost:${PORT}`)
