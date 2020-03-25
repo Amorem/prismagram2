b7d8f5766e509b8af97ee52e56738962a250f3c2
Amorem <dev@amorem.com>
2020-03-25 16:40:23 +0100

    Post isLiked

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -3,17 +3,33 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   User: {
     fullName: parent => `${parent.firsName} ${parent.lastName}`,
-    amIFollowing: (parent, _, { request }) => {
+    isFollowing: (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
       return prisma.$exists.user({
         AND: [{ id: parentId }, { followers_some: { id: user.id } }]
       });
     },
-    itsMe: (parent, _, { request }) => {
+    isSelf: (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
       return user.id === parentId;
+    }
+  },
+  Post: {
+    isLiked: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: { id: user.id }
+          },
+          {
+            post: { id }
+          }
+        ]
+      });
     }
   }
 };
