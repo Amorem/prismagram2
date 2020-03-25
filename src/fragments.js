07136cdc02d5919fb851e03c2b70b950cc2a2f91
Amorem <dev@amorem.com>
2020-03-25 18:52:59 +0100

    seeFullPost

--- src/fragments.js
+++ src/fragments.js
@@ -1,9 +1,48 @@
 export const COMMENT_FRAGMENT = `
-fragment CommentParts on Comment {
-    id
-    text
-    user {
+    fragment CommentParts on Comment {
+        id
+        text
+        user {
+            ${USER_FRAGMENT}
+        }
+    }
+`;
+
+export const USER_FRAGMENT = `
+    fragment Userparts on User {
+        id
         username
     }
-}
+`;
+
+export const FILE_FRAGMENT = `
+    fragment FileParts on File {
+        id
+        url
+    }
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post {
+        id
+        location
+        caption    
+        files {
+            id
+            url
+
+        }
+        comments {
+            id
+            text
+            user {
+                id
+                username
+            }
+        }
+        user {
+           id
+           username
+        }
+    }
 `;
