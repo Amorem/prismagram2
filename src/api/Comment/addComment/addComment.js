3ac11926dbee610dde7e788f156196c4fd341a54
Amorem <dev@amorem.com>
2020-04-04 12:24:12 +0200

    addComment now use Fragment to return User

--- src/api/Comment/addComment/addComment.js
+++ src/api/Comment/addComment/addComment.js
@@ -1,17 +1,22 @@
 import { isAuthenticated } from "../../../middlewares";
 import { prisma } from "../../../../generated/prisma-client";
+import { COMMENT_FRAGMENT } from "../../../fragments";
 
 export default {
   Mutation: {
     addComment: async (_, args, { request }) => {
+      console.log(args);
       isAuthenticated(request);
       const { user } = request;
       const { text, postId } = args;
-      const comment = await prisma.createComment({
-        user: { connect: { id: user.id } },
-        post: { connect: { id: postId } },
-        text
-      });
+      const comment = await prisma
+        .createComment({
+          user: { connect: { id: user.id } },
+          post: { connect: { id: postId } },
+          text
+        })
+        .$fragment(COMMENT_FRAGMENT);
+      console.log(comment);
       return comment;
     }
   }
